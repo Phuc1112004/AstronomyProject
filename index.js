dea253de8b6dfa507db165dd7724a71b53fe46ec
Phuc1112004 <115718926+Phuc1112004@users.noreply.github.com>
2023-03-04 11:15:38 +0700

    .

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.listen(PORT,function (){
     console.log(`Server is running on port ${PORT}`);
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get("/api/authors",function (req,res){           //gọi ra theo đường link
     var sql="select* from nhom4_authors";
@@ -65,3 +71,59 @@ app.get("/api/planet",function (req,res){           //gọi ra theo đường li
         else res.send(data);
     });
 });
+
+app.get("/api/categories/detail",function (req,res){
+    var id = req.query.id;
+    var sql = `select * from nhom4_categories where id = ${id}`;
+    conn.query(sql,function (err,data){
+        if(err) res.send(err);
+        else if(data.length > 0)
+            res.send(data[0]);
+        else res.send("404 not found");
+    });
+});
+
+app.get("/api/authors/detail",function (req,res){
+    var id = req.query.id;
+    var sql = `select * from nhom4_authors where id = ${id}`;
+    conn.query(sql,function (err,data){
+        if(err) res.send(err);
+        else if(data.length > 0)
+            res.send(data[0]);
+        else res.send("404 not found");
+    });
+});
+app.get("/api/images/detail",function (req,res){
+    var id = req.query.id;
+    var sql = `select * from nhom4_images where id = ${id}`;
+    conn.query(sql,function (err,data){
+        if(err) res.send(err);
+        else if(data.length > 0)
+            res.send(data[0]);
+        else res.send("404 not found");
+    });
+});
+
+app.get("/api/planet/detail",function (req,res){
+    var id = req.query.id;
+    var sql = `select * from nhom4_planet where id = ${id}`;
+    conn.query(sql,function (err,data){
+        if(err) res.send(err);
+        else if(data.length > 0)
+            res.send(data[0]);
+        else res.send("404 not found");
+    });
+});
+app.get("/api/posts/detail",function (req,res){
+    var id = req.query.id;
+    var sql = `select * from nhom4_posts where id = ${id}`;
+    conn.query(sql,function (err,data){
+        if(err) res.send(err);
+        else if(data.length > 0)
+            res.send(data[0]);
+        else res.send("404 not found");
+    });
+});
+
+
+
